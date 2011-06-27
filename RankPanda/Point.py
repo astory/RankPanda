a1bc6b8615f2a2ed666dcbf6b957fcf205cded65
Alec Story <avs38@cornell.edu>
2011-06-26 22:21:20 -0700

    tests for Point

--- RankPanda/Point.py
+++ RankPanda/Point.py
@@ -2,10 +2,18 @@ import math
 
 """A simple point module.
 
+Exported Methods:
+
+Distance -- Euclidian distance between two points
+
 Exported Classes:
 
 Point -- a simple point class with an x and y position.
 """
+
+def Distance(p1, p2):
+    """Get the Euclidean distance between two points"""
+    return math.sqrt((p1.x - p2.x)**2 + (p1.y - p2.y)**2)
 
 # TODO(astory): differentiate slopes and points.  They shouldn't be represented
 # with the same class because not all of the functions make sense.
@@ -25,7 +33,6 @@ class Point(object):
         self.x = x
         self.y = y
 
-    # This should be called equals
     def CompareTo(self, p1, epsilon=0.5):
         """Compare two points, and return if they are within epsilon
 
@@ -37,8 +44,7 @@ class Point(object):
             A boolean, whether the points are within epsilon of each other or
             not.
         """
-        return ((math.sqrt((self.x - p1.x)*(self.x - p1.x) +
-                (self.y - p1.y)*(self.y - p1.y))) < epsilon)
+        return Distance(self, p1) < epsilon
 
     def Clone(self):
         """Produce a deep copy of this point"""
---
+++ RankPanda/Point_test.py
@@ -0,0 +1,30 @@
+import unittest
+
+import Point
+
+"""Unit tests for the Point module"""
+
+class TestPoint(unittest.TestCase):
+
+    def test_CompareTo(self):
+        p1 = Point.Point(0.0, 0.0)
+        p2 = Point.Point(0.0, 0.0)
+        p3 = Point.Point(0.0, 1.0)
+        p4 = Point.Point(1.0, 0.0)
+        p5 = Point.Point(0.5, 0.5)
+        p6 = Point.Point(0.1, 0.1)
+
+        self.assertTrue(p1.CompareTo(p2))
+        self.assertTrue(p2.CompareTo(p1))
+        self.assertTrue(p1.CompareTo(p6))
+        self.assertTrue(p6.CompareTo(p1))
+
+        self.assertFalse(p1.CompareTo(p3))
+        self.assertFalse(p3.CompareTo(p1))
+        self.assertFalse(p1.CompareTo(p4))
+        self.assertFalse(p4.CompareTo(p1))
+        self.assertFalse(p1.CompareTo(p5))
+        self.assertFalse(p5.CompareTo(p1))
+
+if __name__ == '__main__':
+    unittest.main()
